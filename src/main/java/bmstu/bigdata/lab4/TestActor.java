f0dbcc4e68381aa8c507f152cdc099a1406298b8
avonamardba <avonamardba@gmail.com>
2021-12-09 21:21:15 +0300

    Scripted auto-commit on change (2021-12-09 21:21:15) by gitwatch.sh

--- src/main/java/bmstu/bigdata/lab4/TestActor.java
+++ src/main/java/bmstu/bigdata/lab4/TestActor.java
@@ -1,4 +1,17 @@
 package bmstu.bigdata.lab4;
 
-public class TestActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+
+public class TestActor extends AbstractActor {
+    private ActorRef storageActor;
+
+    TestActor(ActorRef storageActor) {
+        this.storageActor = storageActor;
+    }
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
