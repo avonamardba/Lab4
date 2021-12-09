95c1824b9f61490fe25ac2e6ee29de6794d5e2d7
avonamardba <avonamardba@gmail.com>
2021-12-09 22:01:48 +0300

    Scripted auto-commit on change (2021-12-09 22:01:48) by gitwatch.sh

--- src/main/java/bmstu/bigdata/lab4/TestPackage.java
+++ src/main/java/bmstu/bigdata/lab4/TestPackage.java
@@ -1,8 +1,26 @@
 package bmstu.bigdata.lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestPackage {
     private String packageId;
     private String functionName;
     private String jsScript;
+    private ArrayList<TestMessage> tests;
+
+    public TestPackage(@JsonProperty("packageId") String packageId,
+                       @JsonProperty("functionName") String functionName,
+                       @JsonProperty("jsScript") String jsScript,
+                       @JsonProperty("tests") ArrayList<TestMessage> tests) {
+        this.packageId = packageId;
+        this.functionName = functionName;
+        this.jsScript = jsScript;
+        this.tests = tests;
+    }
+
+    
 
 }
