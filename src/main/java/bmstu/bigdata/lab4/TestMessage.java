0b8bbf7279894e4a4652d71c6b84dbd1649f2e9c
avonamardba <avonamardba@gmail.com>
2021-12-09 21:32:31 +0300

    Scripted auto-commit on change (2021-12-09 21:32:31) by gitwatch.sh

--- src/main/java/bmstu/bigdata/lab4/TestMessage.java
+++ src/main/java/bmstu/bigdata/lab4/TestMessage.java
@@ -1,5 +1,7 @@
 package bmstu.bigdata.lab4;
 
 public class TestMessage {
-    
+    private TestPackage parentPackage;
+    private String testName;
+    private String results
 }
---
+++ src/main/java/bmstu/bigdata/lab4/TestPackage.java
@@ -0,0 +1,8 @@
+package bmstu.bigdata.lab4;
+
+public class TestPackage {
+    private String packageId;
+    private String functionName;
+    private String jsScript;
+
+}
