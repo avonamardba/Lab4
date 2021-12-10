9640f0ed06cd0e62be3c6bf970310d3f3abdc2ec
avonamardba <avonamardba@gmail.com>
2021-12-10 22:24:28 +0300

    Scripted auto-commit on change (2021-12-10 22:24:28) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/StorageActor.java
+++ src/main/java/bigdata/labs/lab4/StorageActor.java
@@ -1,10 +1,28 @@
 package bigdata.labs.lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StorageActor extends AbstractActor {
+    private final HashMap<String, ArrayList<TestMessage>> storage = new HashMap<>();
+
+    private void putTest(TestMessage testMessage) {
+        String packageId = testMessage.getParent().getPackageId();
+        if (this.storage.containsKey(packageId)) {
+            this.storage.get(packageId).add(testMessage);
+        } else {
+            ArrayList<TestMessage> tests = new ArrayList<>();
+            tests.add(testMessage);
+            this.storage.put(packageId, tests);
+        }
+    }
+
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create().match(TestMessage.class, test -> this.putTest(test)).match(String.class, id -> sender().tell());
     }
 }
