cb07aaa84a8606c1e401db62e6b9952fa5d87af0
avonamardba <avonamardba@gmail.com>
2021-12-10 22:31:17 +0300

    Scripted auto-commit on change (2021-12-10 22:31:17) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/TestResult.java
+++ src/main/java/bigdata/labs/lab4/TestResult.java
@@ -1,2 +1,19 @@
-package bigdata.labs.lab4;public class TestResult {
+package bigdata.labs.lab4;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TestResult {
+    private String actualResult;
+    private String expectedResult;
+    private Boolean success;
+
+    @JsonCreator
+    public TestResult(@JsonProperty("expectedResult") String expectedResult,
+                      @JsonProperty("actualResult") String actualResult,
+                      @JsonProperty("success") Boolean success) {
+        this.expectedResult = expectedResult;
+        this.actualResult = actualResult;
+        this.success = success;
+    }
 }
