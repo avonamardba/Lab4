e988bcde1a6bca1f0356a63974f6a466187d51fd
avonamardba <avonamardba@gmail.com>
2021-12-10 22:53:39 +0300

    Scripted auto-commit on change (2021-12-10 22:53:39) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/RouterActor.java
+++ src/main/java/bigdata/labs/lab4/RouterActor.java
@@ -1,8 +1,15 @@
 package bigdata.labs.lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.SupervisorStrategy;
 
 public class RouterActor extends AbstractActor {
+    private ActorRef storageActor;
+    private ActorRef testActor;
+    private SupervisorStrategy strategy;
+
+    
     @Override
     public Receive createReceive() {
         return null;
