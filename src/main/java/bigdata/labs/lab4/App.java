590786158104b7e7baf4922654152891242af33f
avonamardba <avonamardba@gmail.com>
2021-12-11 00:00:27 +0300

    Scripted auto-commit on change (2021-12-11 00:00:27) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/App.java
+++ src/main/java/bigdata/labs/lab4/App.java
@@ -1,18 +1,36 @@
 package bigdata.labs.lab4;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 import scala.concurrent.Future;
 import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 
+import java.util.concurrent.CompletionStage;
+
 import static akka.http.javadsl.server.PathMatchers.segment;
 
 public class App extends AllDirectives {
     private static final int TIMEOUT = 3000;
 
-    private Route createRoute(ActorRef routerActor) {
+    private final ActorRef routerActor;
+
+    private App(ActorRef routerActor) {
+        this.routerActor = routerActor;
+    }
+
+    private Route createRoute() {
         return concat(
                 get(
                         () -> pathPrefix("getPackage",
@@ -22,6 +40,25 @@ public class App extends AllDirectives {
                                             return completeOKWithFuture(result, Jackson.marshaller());
                                         }
                                 ))),
-                post(() ->));
+                post(
+                        () -> path("postPackage",
+                                () -> entity(Jackson.unmarshaller(TestPackage.class),
+                                        testPackage -> {
+                                            routerActor.tell(testPackage, ActorRef.noSender());
+                                            return complete("Start tests");
+                                        }))));
+    }
+
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("App");
+        ActorRef routerActor = system.actorOf(Props.create(RouterActor.class, system), "routerActor");
+
+        Http http = Http.get(system);
+        ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        App instance = new App(routerActor);
+
+        Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute().flow(system, materializer);
+        CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost());
     }
 }
