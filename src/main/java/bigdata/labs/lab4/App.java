660f223da23cb65a7004cd5f76aa6c7ecb650540
avonamardba <avonamardba@gmail.com>
2021-12-10 23:28:43 +0300

    Scripted auto-commit on change (2021-12-10 23:28:43) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/App.java
+++ src/main/java/bigdata/labs/lab4/App.java
@@ -1,8 +1,10 @@
 package bigdata.labs.lab4;
 
 import akka.actor.ActorRef;
+import akka.compat.Future;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 
 import static akka.http.javadsl.server.PathMatchers.segment;
 
@@ -12,7 +14,7 @@ public class App extends AllDirectives {
                 get(
                         () -> pathPrefix("getPackage",
                                 () -> path(segment(), (String id) -> {
-
+                                    Future<Object> result = Patterns.ask(routerActor, id, )
                                 })))
         )
     }
