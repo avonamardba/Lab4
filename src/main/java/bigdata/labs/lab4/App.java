5c3f57d4a22497113da543d21c0f162f339c8968
avonamardba <avonamardba@gmail.com>
2021-12-10 23:26:26 +0300

    Scripted auto-commit on change (2021-12-10 23:26:26) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/App.java
+++ src/main/java/bigdata/labs/lab4/App.java
@@ -4,8 +4,16 @@ import akka.actor.ActorRef;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 
+import static akka.http.javadsl.server.PathMatchers.segment;
+
 public class App extends AllDirectives {
     private Route createRoute(ActorRef routerActor) {
-        return concat()
+        return concat(
+                get(
+                        () -> pathPrefix("getPackage",
+                                () -> path(segment(), (String id) -> {
+
+                                })))
+        )
     }
 }
