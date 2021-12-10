f4c6114799836c8440dec52922f3c71e0b64f3be
avonamardba <avonamardba@gmail.com>
2021-12-10 23:29:16 +0300

    Scripted auto-commit on change (2021-12-10 23:29:16) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/App.java
+++ src/main/java/bigdata/labs/lab4/App.java
@@ -9,12 +9,13 @@ import akka.pattern.Patterns;
 import static akka.http.javadsl.server.PathMatchers.segment;
 
 public class App extends AllDirectives {
+    private static final int TIMEOUT = 3000;
     private Route createRoute(ActorRef routerActor) {
         return concat(
                 get(
                         () -> pathPrefix("getPackage",
                                 () -> path(segment(), (String id) -> {
-                                    Future<Object> result = Patterns.ask(routerActor, id, )
+                                    Future<Object> result = Patterns.ask(routerActor, id, TIMEOUT)
                                 })))
         )
     }
