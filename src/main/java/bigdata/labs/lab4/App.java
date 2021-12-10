45d9d6d84dd82ccdd8ba220884c29cbc44324ac1
avonamardba <avonamardba@gmail.com>
2021-12-10 23:33:25 +0300

    Scripted auto-commit on change (2021-12-10 23:33:25) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/App.java
+++ src/main/java/bigdata/labs/lab4/App.java
@@ -2,6 +2,7 @@ package bigdata.labs.lab4;
 
 import akka.actor.ActorRef;
 import akka.compat.Future;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
@@ -10,13 +11,16 @@ import static akka.http.javadsl.server.PathMatchers.segment;
 
 public class App extends AllDirectives {
     private static final int TIMEOUT = 3000;
+
     private Route createRoute(ActorRef routerActor) {
         return concat(
                 get(
                         () -> pathPrefix("getPackage",
                                 () -> path(segment(), (String id) -> {
-                                    Future<Object> result = Patterns.ask(routerActor, id, TIMEOUT)
-                                })))
-        )
+                                            Future<Object> result = Patterns.ask(routerActor, id, TIMEOUT);
+                                            return completeOKWithFuture(result, Jackson.marshaller());
+                                        }
+                                ))),
+                post(() ->));
     }
 }
