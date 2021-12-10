1e1745fc6542ba8c4a181f129dc4a63eb16a8c9c
avonamardba <avonamardba@gmail.com>
2021-12-10 23:20:59 +0300

    Scripted auto-commit on change (2021-12-10 23:20:59) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/App.java
+++ src/main/java/bigdata/labs/lab4/App.java
@@ -1,10 +1,34 @@
 package bigdata.labs.lab4;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import scala.concurrent.Future;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
+import static akka.http.javadsl.server.Directives.route;
 
 public class App {
     private Route createRoute(ActorRef routerActor) {
-        return route
+        return route();
     }
 }
