ca5946a07b612b3171b1e14c71bac77f082fe07e
avonamardba <avonamardba@gmail.com>
2021-12-10 23:03:15 +0300

    Scripted auto-commit on change (2021-12-10 23:03:15) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/App.java
+++ src/main/java/bigdata/labs/lab4/App.java
@@ -1,7 +1,10 @@
 package bigdata.labs.lab4;
 
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 
 public class App {
-    
+    private Route createRoute() {
+        return concat()
+    }
 }
