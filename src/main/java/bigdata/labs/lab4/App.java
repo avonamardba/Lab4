41743d261c779bc415564b68aa245b0aba3ace69
avonamardba <avonamardba@gmail.com>
2021-12-11 12:50:14 +0300

    Scripted auto-commit on change (2021-12-11 12:50:14) by gitwatch.sh

--- src/main/java/bigdata/labs/lab4/App.java
+++ src/main/java/bigdata/labs/lab4/App.java
@@ -1,4 +1,5 @@
-package bigdata.labs.lab4;
+package bmstu.bigdata.lab4;
+
 
 import akka.NotUsed;
 import akka.actor.ActorRef;
@@ -6,70 +7,18 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
-import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-import scala.concurrent.Future;
-import akka.http.javadsl.marshallers.jackson.Jackson;
-import akka.http.javadsl.server.AllDirectives;
-import akka.http.javadsl.server.Route;
-import akka.pattern.Patterns;
 
-import java.io.IOException;
-import java.util.concurrent.CompletionStage;
-
-import static akka.http.javadsl.server.PathMatchers.segment;
-
-public class App extends AllDirectives {
-    private static final int TIMEOUT = 3000;
-    private static final String ACTOR_SYSTEM_NAME = "App";
-    private static final String HOSTNAME = "localhost";
-    private static final int PORT = 8080;
-
-
-    private final ActorRef routerActor;
-
-    private App(ActorRef routerActor) {
-        this.routerActor = routerActor;
-    }
-
-    public Route createRoute() {
-        return path("testapp", () -> route(
-                get(() -> parameter("packageId", packageId -> {
-                    RequestAnswers msg = new RequestAnswers(packageId);
-                    Future<Object> result = Patterns.ask(routerActor, msg, 5000);
-                    return completeOKWithFuture(result, Jackson.marshaller());
-                })),
-                post(() ->
-                        entity(Jackson.unmarshaller(TestActor.class), msg -> {
-                            Patterns.ask(routerActor, msg, 5000);
-                            return complete("ok");
-                        })
-                )
-        ));
-    }
-
-    public static void main(String[] args) throws IOException {
-        ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
-        ActorRef routerActor = system.actorOf(Props.create(RouterActor.class, system));
-
+public class TestApp {
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("javascript_code_test");
+        ActorRef router = system.actorOf(Props.create(Router.class));
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-
-        App instance = new App(routerActor);
-
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute().flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-                routeFlow,
-                ConnectHttp.toHost(HOSTNAME, PORT),
-                materializer);
-
-        String startMessage = String.format("Server started at http://%s:%d/\\", HOSTNAME, PORT);
-        System.out.println(startMessage);
-        System.in.read();
-        binding.thenCompose(serverBinding -> serverBinding.unbind())
-                .thenAccept(unbound -> system.terminate());
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = new HTTPRoute(router).createRoute().flow(system, materializer);
+        http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8081), materializer);
     }
 }
